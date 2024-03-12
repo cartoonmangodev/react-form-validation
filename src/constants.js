e1117ee66d5ec7c7a0bb893e848ba2f3ddd21c17
chrissiefdo <chrissie@cartoonmango.com>
2024-03-13 04:36:01 +0530

    [MODIFIED]

--- src/constants.js
+++ src/constants.js
@@ -4,6 +4,7 @@ export const TYPE_STRING = "string";
 export const TYPE_ARRAY = "array";
 export const TYPE_BOOLEAN = "boolean";
 export const TYPE_OBJECT = "object";
+export const TYPE_NUMBER = "number";
 export const TYPE_FUNCTION = "function";
 export const TYPE_ERROR = "error";
 export const TYPE_SYMBOL = "symbol";
